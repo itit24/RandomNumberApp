f19f013352637cba4bedade51b01813f562ee3c5
antonio <tsosa24@gmail.com>
2016-11-11 12:55:25 -0600

    se muestra toast con numero random

--- app/src/main/java/com/nihao1234/randomnumberapp/MainActivity.java
+++ app/src/main/java/com/nihao1234/randomnumberapp/MainActivity.java
@@ -1,7 +1,12 @@
+//linea nueva
 package com.nihao1234.randomnumberapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // instancia de la calse random
+        Random rand = new Random();
+        //asignar variable random de un numero entero
+        int n = rand.nextInt(20);
+
+        Context context = getApplicationContext();
+        String number = n + "numero";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context,number,duration);
+        toast.show();
+
     }
 }
